43e4fe112c1df633b301919f8212971baca79d09
Gu Zeng <zeng.gu@iaspec.net>
2014-09-09 11:34:35 +0800

    内容截取修改

--- app/start/global.php
+++ app/start/global.php
@@ -176,12 +176,51 @@ function timezones($tz = '')
 
 function utf8_strcut($str, $length, $start=0)
 {
+    $str = noHTML(trim($str));
     preg_match_all('/./us', $str, $match);  
     $chars = is_null($length)? array_slice($match[0], $start ) : array_slice($match[0], $start, $length);  
     unset($str);
     return implode('', $chars);
 }
 
+function noHTML($content)
+{
+    $content = str_replace("&nbsp;",'', $content);  
+    $content = preg_replace("/<a[^>]*>/i",'', $content);  
+    $content = preg_replace("/<\/a>/i", '', $content);   
+    $content = preg_replace("/<div[^>]*>/i",'', $content);  
+    $content = preg_replace("/<\/div>/i",'', $content);
+    $content = preg_replace("/<font[^>]*>/i",'', $content);  
+    $content = preg_replace("/<\/font>/i",'', $content);
+    $content = preg_replace("/<p[^>]*>/i",'', $content);  
+    $content = preg_replace("/<\/p>/i",'', $content);
+    $content = preg_replace("/<span[^>]*>/i",'', $content);  
+    $content = preg_replace("/<\/span>/i",'', $content);
+    $content = preg_replace("/<\?xml[^>]*>/i",'', $content);
+    $content = preg_replace("/<\/\?xml>/i",'', $content);
+    $content = preg_replace("/<o:p[^>]*>/i",'', $content);
+    $content = preg_replace("/<\/o:p>/i",'', $content);
+    $content = preg_replace("/<u[^>]*>/i",'', $content);
+    $content = preg_replace("/<\/u>/i",'', $content);
+    $content = preg_replace("/<b[^>]*>/i",'', $content);
+    $content = preg_replace("/<\/b>/i",'', $content);
+    $content = preg_replace("/<meta[^>]*>/i",'', $content);
+    $content = preg_replace("/<\/meta>/i",'', $content);
+    $content = preg_replace("/<!--[^>]*-->/i",'', $content);//注释内容 
+    $content = preg_replace("/<p[^>]*-->/i",'', $content);//注释内容      
+    $content = preg_replace("/style=.+?['|\"]/i",'',$content);//去除样式  
+    $content = preg_replace("/class=.+?['|\"]/i",'',$content);//去除样式  
+    $content = preg_replace("/id=.+?['|\"]/i",'',$content);//去除样式     
+    $content = preg_replace("/lang=.+?['|\"]/i",'',$content);//去除样式      
+    $content = preg_replace("/width=.+?['|\"]/i",'',$content);//去除样式   
+    $content = preg_replace("/height=.+?['|\"]/i",'',$content);//去除样式   
+    $content = preg_replace("/border=.+?['|\"]/i",'',$content);//去除样式   
+    $content = preg_replace("/face=.+?['|\"]/i",'',$content);//去除样式
+    $content = preg_replace("/face=.+?['|\"]/",'',$content);
+    $content = preg_replace("/face=.+?['|\"]/",'',$content);
+    $content=str_replace( "&nbsp;","",$content);
+    return $content;
+}
 /*
 |--------------------
 | 注册自定义的验证器扩展
