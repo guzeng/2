82d5f1ecdf12bba13f112aec72a78f0d4496e1e2
Gu Zeng <zeng.gu@iaspec.net>
2014-09-12 17:39:50 +0800

    首页修改

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -7,8 +7,8 @@ class IndexController extends BaseController {
 
 		$data['allType'] = Order::getType();
 		$data['allCity'] = City::all();
-        $data['faq_list'] = News::where('status', 1)->where('category_id',2)->orderBy('open_time', 'desc')->take(5)->get();
-        $data['news_list'] = News::where('status', 1)->where('category_id',3)->orderBy('open_time', 'desc')->take(5)->get();
+        $data['faq_list'] = News::where('status', 1)->where('category_id',2)->orderBy('id', 'asc')->take(5)->get();
+        $data['news_list'] = News::where('status', 1)->where('category_id',3)->orderBy('id', 'desc')->take(5)->get();
 		return View::make('home.index',$data);
 	}
 
