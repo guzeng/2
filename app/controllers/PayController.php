9bf533b1a9d0b738bc853b53dfdf71bb4269afd7
Gu Zeng <zeng.gu@iaspec.net>
2014-09-30 14:25:01 +0800

    修改支付接口

--- app/controllers/PayController.php
+++ app/controllers/PayController.php
@@ -58,11 +58,11 @@ class PayController extends BaseController {
         $payment_type = "1";
         //必填，不能修改
         //服务器异步通知页面路径
-        $notify_url = "http://www.yuexing.com/pay/aliapy-notify";
+        $notify_url = "http://www.yuexingtrip.com/pay/aliapy-notify";
         //需http://格式的完整路径，不能加?id=123这类自定义参数
 
         //页面跳转同步通知页面路径
-        $return_url = "http://www.yuexing.com/pay/alipay-return";
+        $return_url = "http://www.yuexingtrip.com/pay/alipay-return";
         //需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
 
         //卖家支付宝帐户
