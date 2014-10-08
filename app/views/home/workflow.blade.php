53d4d35794beaa3213435157769ee9036aff3b4a
Gu Zeng <zeng.gu@iaspec.net>
2014-10-08 16:57:46 +0800

    英文版修改

--- app/views/home/workflow.blade.php
+++ app/views/home/workflow.blade.php
@@ -23,11 +23,29 @@
               <div class="row">
                 <!-- BEGIN LEFT SIDEBAR -->            
                 <div class="col-md-9 col-sm-9 blog-posts">
-                    <h3 class='text-center'>行李运送流程</h3>
+                    <h3 class='text-center'><?php echo App::getLocale()=='zh' ? '行李运送流程' : 'luggage transport process';?></h3>
                     <div class='m-t-50'>
-                        <p><strong>1、</strong>在网上自助下单，点击“开始预订”后填写您的相关信息，或者通过电话向网站客服人员下单。</p>
-                        <p><strong>2、</strong>如果您的行李是从机场运送至目的地，航班落地后，我们的配送员将第一时间联系您，并在机场到达厅接收您的行李，填写托运单据。</p>
-                        <p><strong>3、</strong>如果您的行李是从住宅、酒店、或者商业大厦等地点运送至机场，请在您的航班起飞前至少6小时下单，我们的配送员将上门收取行李，并在航班起飞前1.5小时将您的行李运送至机场，以便您领取。</p>
+                        <p><strong>1. </strong>
+                            <?if(App::getLocale()=='zh'):?>
+                            在网上自助下单，点击“开始预订”后填写您的相关信息，或者通过电话向网站客服人员下单。
+                            <?else:?>
+                            At online self-help order, click on "start on order” and fill in your information, or order by phoning the on line customer service staff.
+                            <?endif;?>
+                        </p>
+                        <p><strong>2. </strong>
+                            <?if(App::getLocale()=='zh'):?>
+                            如果您的行李是从机场运送至目的地，航班落地后，我们的配送员将第一时间联系您，并在机场到达厅接收您的行李，填写托运单据。
+                            <?else:?>
+                            If your luggage is transported from airport to the destination, after the flight lands, our deliveryman will contact you the first time, receives your luggage at the airport arrival hall, and fill in the shipping documents.
+                            <?endif;?>
+                        </p>
+                        <p><strong>3. </strong>
+                            <?if(App::getLocale()=='zh'):?>
+                            如果您的行李是从住宅、酒店、或者商业大厦等地点运送至机场，请在您的航班起飞前至少6小时下单，我们的配送员将上门收取行李，并在航班起飞前1.5小时将您的行李运送至机场，以便您领取。
+                            <?else:?>
+                            If your luggage is carried from places such as residence, hotel, or commercial building, to the airport, please drop the order at least 6 hours before your departure, our deliveryman will collect luggage from door to door, and get back luggage which is carried to the departure hall 1.5 hourse before flight departs.
+                            <?endif;?>
+                        </p>
                     </div>
                 </div>
                 <!-- END LEFT SIDEBAR -->
