ace0a414d05f41aa927bb221d2046c846cf1b6f2
Gu Zeng <zeng.gu@iaspec.net>
2014-10-15 15:45:16 +0800

    baidu tongji

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -443,8 +443,13 @@
 		            });
 				});
 				</script>
-		<!----//End-footer---->
-		<!-----//End-container---->
+				<script type="text/javascript">
+				var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+				document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F338c853439464c19e18a201f0a7f895c' type='text/javascript'%3E%3C/script%3E"));
+				</script>
+
+		<!--End-footer-->
+		<!--End-container-->
 
 </body>
 </html>
